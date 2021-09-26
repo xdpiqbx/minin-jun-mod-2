83a6568a8f4743edb02c6513ceb38f45ac92191d
xdpiqbx <xdpiqbx@gmail.com>
2021-09-26 19:27:21 +0300

    006 all done

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropType from 'prop-types'
 import Table from './Table'
 import BookMark from './BookMark'
@@ -6,7 +7,11 @@ import QualitiesList from './QualitiesList'
 
 const UsersTable = ({ users, onSort, selectedSort, onToggleBookmark, onDelete }) => {
   const columns = {
-    name: { path: 'name', name: 'Имя' },
+    name: {
+      path: 'name',
+      name: 'Имя',
+      component: (user) => <Link to={`/users/${user._id}`}>{user.name}</Link>
+    },
     qualities: {
       name: 'Качества',
       component: (user) => <QualitiesList qualities={user.qualities} />
