ec2d96f306f14dceca3c5282cc82f8af146f0249
xdpiqbx <xdpiqbx@gmail.com>
2021-10-16 22:15:25 +0300

    lessons in 009-1 done

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const getInputClasses = () => {
+    return `form-control ` + (error ? 'is-invalid' : 'is-valid')
+  }
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState)
+  }
   return (
-    <div>
+    <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input type={type} id={name} value={value} onChange={onChange} name={name} />
-      {error && <p>{error}</p>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? 'text' : type}
+          id={name}
+          value={value}
+          onChange={onChange}
+          name={name}
+          className={getInputClasses()}
+        />
+        {type === 'password' && (
+          <button className="btn btn-outline-secondary" type="button" onClick={toggleShowPassword}>
+            <i className={'bi bi-eye' + (showPassword ? '-slash' : '')}></i>
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
     </div>
   )
 }
