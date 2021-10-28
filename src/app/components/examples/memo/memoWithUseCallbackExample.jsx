16d25089591864de44c87cff5efc0075ba51522a
xdpiqbx <xdpiqbx@gmail.com>
2021-10-28 08:31:51 +0300

    React.memo start from 6:35

--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,28 @@
+import React, { useEffect } from 'react'
+
+import PropTypes from 'prop-types'
+
+const LogOutBtn = ({ onLogOut }) => {
+  useEffect(() => {
+    console.log('render LogOutBtn')
+  })
+
+  return (
+    <button className="btn btn-primary" onClick={onLogOut}>
+      LogOutBtn
+    </button>
+  )
+}
+
+LogOutBtn.propTypes = {
+  onLogOut: PropTypes.func
+}
+
 const MemoWithUseCallbackExample = (props) => {
-  return null
+  const handleLogOut = () => {
+    localStorage.removeItem('auth')
+  }
+  return <LogOutBtn onLogOut={handleLogOut} />
 }
 
 export default MemoWithUseCallbackExample
