57c3f3f4ddfba723513d3afac66a7f78b106b083
xdpiqbx <xdpiqbx@gmail.com>
2021-12-01 08:33:20 +0200

    video 6 done to 015-br

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,18 +1,3 @@
-# 15. Авторизация и аутентификация. Часть 1
-
-## 4. Рефакторинг формы регистрации - DONE
-
-## 5. Принципы регистрации в Firebase
-
-Мой тестовый `pwd: test1234`
-
-[Firebase Auth REST API](https://firebase.google.com/docs/reference/rest/auth)
-
-[Sign up with email / password](https://firebase.google.com/docs/reference/rest/auth#section-create-email-password)
-
-## 6. Auth Provider - просто БАЗА
-
-```jsx
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 // import userService from "../services/user.service";
@@ -36,4 +21,3 @@ AuthProvider.propTypes = {
 };
 
 export default AuthProvider;
-```
