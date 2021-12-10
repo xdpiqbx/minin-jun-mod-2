0395b18c7110607a1f895fec1f8fd10ac25642e1
xdpiqbx <xdpiqbx@gmail.com>
2021-12-10 08:19:40 +0200

    module 15 done

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -6,90 +6,102 @@ import { toast } from "react-toastify";
 import { setTokens } from "../services/localstorage.service";
 
 const httpAuth = axios.create({
-  baseURL: "https://identitytoolkit.googleapis.com/v1/",
-  params: {
-    key: process.env.REACT_APP_FIREBASE_KEY
-  }
+    baseURL: "https://identitytoolkit.googleapis.com/v1/",
+    params: {
+        key: process.env.REACT_APP_FIREBASE_KEY
+    }
 });
 
 const AuthContext = React.createContext();
 
 export const useAuth = () => {
-  return useContext(AuthContext);
+    return useContext(AuthContext);
 };
 
 const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({});
-  const [error, setError] = useState(null);
+    const [currentUser, setCurrentUser] = useState({});
+    const [error, setError] = useState(null);
 
-  async function logIn({ email, password }) {
-    const url = `accounts:signInWithPassword`;
-    try {
-      const { data } = await httpAuth.post(url, {
-        email,
-        password,
-        returnSecureToken: true
-      });
-      setTokens(data);
-    } catch (error) {
-      errorCatcher(error);
-    }
-  }
-
-  async function signUp({ email, password, ...rest }) {
-    const url = `accounts:signUp`;
-    try {
-      const { data } = await httpAuth.post(url, {
-        email,
-        password,
-        returnSecureToken: true
-      });
-      setTokens(data);
-      await createUser({ _id: data.localId, email, ...rest });
-    } catch (error) {
-      errorCatcher(error);
-      const { code, message } = error.response.data.error;
-      console.log({ code, message });
-      if (code === 400) {
-        if (message === "EMAIL_EXISTS") {
-          const errorObject = {
-            email: "Пользователь с таким email существует"
-          };
-          throw errorObject;
+    async function logIn({ email, password }) {
+        const url = `accounts:signInWithPassword`;
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
+            setTokens(data);
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            console.log({ code, message });
+            if (code === 400) {
+                switch (message) {
+                    case "INVALID_PASSWORD":
+                        throw new Error("Email или Пароль введены некорректно");
+                    default:
+                        throw new Error(
+                            "Слишком много попыток входа. Давай пожжей"
+                        );
+                }
+            }
         }
-      }
     }
-  }
-  async function createUser(data) {
-    try {
-      const { content } = await userService.create(data);
-      setCurrentUser(content);
-    } catch (error) {
-      errorCatcher(error);
+
+    async function signUp({ email, password, ...rest }) {
+        const url = `accounts:signUp`;
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
+            setTokens(data);
+            await createUser({ _id: data.localId, email, ...rest });
+        } catch (error) {
+            errorCatcher(error);
+            const { code, message } = error.response.data.error;
+            console.log({ code, message });
+            if (code === 400) {
+                if (message === "EMAIL_EXISTS") {
+                    const errorObject = {
+                        email: "Пользователь с таким email существует"
+                    };
+                    throw errorObject;
+                }
+            }
+        }
     }
-  }
-  function errorCatcher(error) {
-    const { message } = error.response.data;
-    setError(message);
-  }
-  useEffect(() => {
-    if (error !== null) {
-      toast(error);
-      setError(null);
+    async function createUser(data) {
+        try {
+            const { content } = await userService.create(data);
+            setCurrentUser(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
     }
-  }, [error]);
-  return (
-    <AuthContext.Provider value={{ signUp, logIn, currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+    }
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+    return (
+        <AuthContext.Provider value={{ signUp, logIn, currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 AuthProvider.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
 };
 
 export default AuthProvider;
